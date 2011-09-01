ajzafar/vim-snipmate

commit 339ab33dc5eff810b6a8bc43d0375bbb3ee73a2e

Fix triggering of snippets in last tab stop

autoload/snipMate.vim
@@ -267,7 +267,7 @@ fun snipMate#jumpTabStop(backwards)
 	if s:curPos == s:snipLen
 		let sMode = s:endCol == g:snipPos[s:curPos-1][1]+g:snipPos[s:curPos-1][2]
 		call s:RemoveSnippet()
-		return sMode ? "\<tab>" : TriggerSnippet()
+		return sMode ? "\<tab>" : ''
 	endif
 
 	call cursor(g:snipPos[s:curPos][0], g:snipPos[s:curPos][1])

plugin/snipMate.vim
@@ -135,7 +135,12 @@ endfunction
 " Snippet triggering/expansion {{{
 
 function! s:TriggerSnippet()
-	if exists('g:snipPos') | return s:NextTabStop() | endif
+	if exists('g:snipPos')
+		let jump = s:NextTabStop()
+		if jump != ''
+			return jump
+		endif
+	endif
 
 	" Grab the trigger (and where it begins)
 	let [trigger, begin] = s:GrabTrigger()
@@ -246,8 +251,7 @@ function! s:PrevTabStop()
 endfunction
 
 function! s:NextTabStop()
-	if exists('g:snipPos') | return snipMate#jumpTabStop(0) | endif
-	return "\<s-tab>"
+	return snipMate#jumpTabStop(0)
 endfunction
 
 " Reload snippets for filetype.

commit 5d3db243361187645a2f9190ab8d1b33c229ed93

Fix jumping to a tab stop without a placeholder
The bug was introduced by 339ab33.

autoload/snipMate.vim
@@ -267,7 +267,7 @@ fun snipMate#jumpTabStop(backwards)
 	if s:curPos == s:snipLen
 		let sMode = s:endCol == g:snipPos[s:curPos-1][1]+g:snipPos[s:curPos-1][2]
 		call s:RemoveSnippet()
-		return sMode ? "\<tab>" : ''
+		return sMode ? "\<tab>" : -1
 	endif
 
 	call cursor(g:snipPos[s:curPos][0], g:snipPos[s:curPos][1])

plugin/snipMate.vim
@@ -137,7 +137,7 @@ endfunction
 function! s:TriggerSnippet()
 	if exists('g:snipPos')
 		let jump = s:NextTabStop()
-		if jump != ''
+		if type(jump) == 1
 			return jump
 		endif
 	endif